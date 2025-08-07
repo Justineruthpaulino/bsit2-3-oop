c9bdecffdeb9e3f474c0a20ebfe6ccd4b3cfeca6
Justineruthpaulino <jrpaulino11855@liceo.edu.ph>
2025-08-07 16:02:54 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+
+        Student s1 = new Student("Tinee", 20, "BSIT", 90, 85, 88);
+        Student s2 = new Student("Pandee", 19, "BSCS", 70, 72, 68);
+        Student s3 = new Student("Yshie", 21, "BSIS", 55, 60, 58);
+
+        Student[] students = {s1, s2, s3};
+        int passingCount = 0;
+
+        for (Student s : students) {
+            System.out.println("");
+            s.displayInfo();
+            double avg = s.calculateAverage();
+            System.out.printf("Average Grade : %.2f\n", avg);
+            System.out.println("Letter Grade  : " + s.getLetterGrade());
+            System.out.println("Status        : " + (s.isPassing() ? "PASSING" : "FAILING"));
+            if (s.isPassing()) {
+                passingCount++;
+            }
+            System.out.println("");
+        }
+
+
+        System.out.println("Summary:");
+        System.out.println("Total students passing: " + passingCount + " out of " + students.length);
     }
 }
